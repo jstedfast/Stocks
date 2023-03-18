421cf2e374a500d365137e3eb8871264a6c900c0
Jeffrey Stedfast <jestedfa@microsoft.com>
2023-03-18 12:01:29 -0400

    Improved StockDetailsPage charting logic

--- Stocks/Views/StockDetailsPage.xaml.cs
+++ Stocks/Views/StockDetailsPage.xaml.cs
@@ -68,7 +68,7 @@ public partial class StockDetailsPage : ContentPage
         };
         StockPriceChart.TooltipFindingStrategy = TooltipFindingStrategy.CompareOnlyX;
         StockPriceChart.DrawMargin = new Margin(10, 5, 70, 30);
-        UpdateXAxis(YahooFinanceTimeRange.OneDay, YahooFinanceTimeInterval.OneMinute, values);
+        UpdateXAxis(YahooFinanceTimeRange.OneDay, values, 0);
         UpdateYAxis(null, null);
 
         foreach (var radio in StockPriceChartTimeRangesLayout.Children.OfType<RadioButton>())
@@ -240,42 +240,18 @@ public partial class StockDetailsPage : ContentPage
 
         return TimeSpan.FromSeconds(value);
     }
-
-#if false
-    static string HourlyLabeler(double ticks)
+    string HourlyLabeler(double x)
     {
-        var utcTimestamp = new DateTime((long)ticks, DateTimeKind.Utc);
-        var timestamp = utcTimestamp.ToLocalTime();
-        var hour = timestamp.Hour;
+        int index = Math.Min(Math.Max((int)x, 0), values.Count - 1);
 
-        if (hour > 12)
-            hour -= 12;
+        if (index < 0)
+            return string.Empty;
 
-        return hour.ToString();
-    }
-
-    static string DailyLabeler(double ticks)
-    {
-        return new DateTime((long)ticks, DateTimeKind.Utc).ToLocalTime().ToString("dd");
-    }
-
-    static string MonthlyLabeler(double ticks)
-    {
-        return new DateTime((long)ticks, DateTimeKind.Utc).ToLocalTime().ToString("MMM");
-    }
-
-    static string YearlyLabeler(double ticks)
-    {
-        return new DateTime((long)ticks, DateTimeKind.Utc).ToLocalTime().ToString("yyyy");
-    }
-#else
-    string HourlyLabeler(double index)
-    {
-        var trade = values[Math.Min(Math.Max((int)index, 0), values.Count - 1)];
+        var trade = values[index];
         var timestamp = trade.Timestamp.ToLocalTime();
         var hour = timestamp.Hour;
 
-        if (index < 0)
+        if (x < 0)
             hour--;
 
         if (hour > 12)
@@ -284,24 +260,41 @@ public partial class StockDetailsPage : ContentPage
         return hour.ToString();
     }
 
-    string DailyLabeler(double index)
+    string DailyLabeler(double x)
     {
-        var trade = values[Math.Min(Math.Max((int)index, 0), values.Count - 1)];
+        int index = Math.Min(Math.Max((int)x, 0), values.Count - 1);
+
+        if (index < 0)
+            return string.Empty;
+
+        var trade = values[index];
+
         return trade.Timestamp.ToLocalTime().ToString("dd");
     }
 
-    string MonthlyLabeler(double index)
+    string MonthlyLabeler(double x)
     {
-        var trade = values[Math.Min(Math.Max((int)index, 0), values.Count - 1)];
+        int index = Math.Min(Math.Max((int)x, 0), values.Count - 1);
+
+        if (index < 0)
+            return string.Empty;
+
+        var trade = values[index];
+
         return trade.Timestamp.ToLocalTime().ToString("MMM");
     }
 
-    string YearlyLabeler(double index)
+    string YearlyLabeler(double x)
     {
-        var trade = values[Math.Min(Math.Max((int)index, 0), values.Count - 1)];
+        int index = Math.Min(Math.Max((int)x, 0), values.Count - 1);
+
+        if (index < 0)
+            return string.Empty;
+
+        var trade = values[index];
+
         return trade.Timestamp.ToLocalTime().ToString("yyyy");
     }
-#endif
 
     Func<double, string> GetXAxesLabeler(YahooFinanceTimeRange range, List<StockTradePoint> values)
     {
@@ -337,6 +330,124 @@ public partial class StockDetailsPage : ContentPage
         }
     }
 
+    static double GetHourlyStepUnits(YahooFinanceTimeInterval interval)
+    {
+        switch (interval)
+        {
+            case YahooFinanceTimeInterval.OneMinute: return 60;
+            case YahooFinanceTimeInterval.TwoMinutes: return 30;
+            case YahooFinanceTimeInterval.FiveMinutes: return 12;
+            case YahooFinanceTimeInterval.FifteenMinutes: return 4;
+            case YahooFinanceTimeInterval.ThirtyMinutes: return 2;
+            case YahooFinanceTimeInterval.SixtyMinutes:
+            case YahooFinanceTimeInterval.OneHour: return 1;
+            case YahooFinanceTimeInterval.NinetyMinutes: return 0.75;
+            default: return 0;
+        }
+    }
+
+    static double GetDailyStepUnits(YahooFinanceTradingPeriod tradingPeriod, YahooFinanceTimeInterval interval)
+    {
+        var duration = tradingPeriod.End - tradingPeriod.Start;
+        var minutesPerDay = duration.TotalMinutes;
+
+        switch (interval)
+        {
+            case YahooFinanceTimeInterval.OneMinute: return minutesPerDay;
+            case YahooFinanceTimeInterval.TwoMinutes: return minutesPerDay / 2;
+            case YahooFinanceTimeInterval.FiveMinutes: return minutesPerDay / 5;
+            case YahooFinanceTimeInterval.FifteenMinutes: return minutesPerDay / 15;
+            case YahooFinanceTimeInterval.ThirtyMinutes: return minutesPerDay / 30;
+            case YahooFinanceTimeInterval.SixtyMinutes:
+            case YahooFinanceTimeInterval.OneHour: return minutesPerDay / 60;
+            case YahooFinanceTimeInterval.NinetyMinutes: return minutesPerDay / 90;
+            case YahooFinanceTimeInterval.OneDay: return minutesPerDay;
+            default: return 0;
+        }
+    }
+
+    static double GetMonthlyStepUnits(YahooFinanceTradingPeriod tradingPeriod, YahooFinanceTimeInterval interval)
+    {
+        var duration = tradingPeriod.End - tradingPeriod.Start;
+        var minutesPerDay = duration.TotalMinutes;
+        const double daysPerMonth = 365.25 / 12;
+        const double tradingDaysPerMonth = (daysPerMonth / 7) * 5;
+        var tradingMinutesPerMonth = minutesPerDay * tradingDaysPerMonth;
+
+        switch (interval)
+        {
+            case YahooFinanceTimeInterval.OneMinute: return tradingMinutesPerMonth;
+            case YahooFinanceTimeInterval.TwoMinutes: return tradingMinutesPerMonth / 2;
+            case YahooFinanceTimeInterval.FiveMinutes: return tradingMinutesPerMonth / 5;
+            case YahooFinanceTimeInterval.FifteenMinutes: return tradingMinutesPerMonth / 15;
+            case YahooFinanceTimeInterval.ThirtyMinutes: return tradingMinutesPerMonth / 30;
+            case YahooFinanceTimeInterval.SixtyMinutes:
+            case YahooFinanceTimeInterval.OneHour: return tradingMinutesPerMonth / 60;
+            case YahooFinanceTimeInterval.NinetyMinutes: return tradingMinutesPerMonth / 90;
+            case YahooFinanceTimeInterval.OneDay: return tradingDaysPerMonth;
+            case YahooFinanceTimeInterval.FiveDays: return tradingDaysPerMonth / 5;
+            default: return 0;
+        }
+    }
+
+    static double GetYearlyStepUnits(YahooFinanceTradingPeriod tradingPeriod, YahooFinanceTimeInterval interval)
+    {
+        var duration = tradingPeriod.End - tradingPeriod.Start;
+        var minutesPerDay = duration.TotalMinutes;
+        const double daysPerYear = 365.25 / 12;
+        const double tradingDaysPerYear = (daysPerYear / 7) * 5;
+        var tradingMinutesPerYear = minutesPerDay * tradingDaysPerYear;
+
+        switch (interval)
+        {
+            case YahooFinanceTimeInterval.OneMinute: return tradingMinutesPerYear;
+            case YahooFinanceTimeInterval.TwoMinutes: return tradingMinutesPerYear / 2;
+            case YahooFinanceTimeInterval.FiveMinutes: return tradingMinutesPerYear / 5;
+            case YahooFinanceTimeInterval.FifteenMinutes: return tradingMinutesPerYear / 15;
+            case YahooFinanceTimeInterval.ThirtyMinutes: return tradingMinutesPerYear / 30;
+            case YahooFinanceTimeInterval.SixtyMinutes:
+            case YahooFinanceTimeInterval.OneHour: return tradingMinutesPerYear / 60;
+            case YahooFinanceTimeInterval.NinetyMinutes: return tradingMinutesPerYear / 90;
+            case YahooFinanceTimeInterval.OneDay: return tradingDaysPerYear;
+            case YahooFinanceTimeInterval.FiveDays: return tradingDaysPerYear / 5;
+            default: return 0;
+        }
+    }
+
+    static double GetXAxesMinStep(YahooFinanceTradingPeriod tradingPeriod, YahooFinanceTimeRange range, YahooFinanceTimeInterval interval, List<StockTradePoint> values)
+    {
+        switch (range)
+        {
+            case YahooFinanceTimeRange.OneDay: return GetHourlyStepUnits(interval);
+            case YahooFinanceTimeRange.FiveDay: return GetDailyStepUnits(tradingPeriod, interval);
+            case YahooFinanceTimeRange.OneMonth: return GetDailyStepUnits(tradingPeriod, interval);
+            case YahooFinanceTimeRange.ThreeMonth: return GetMonthlyStepUnits(tradingPeriod, interval);
+            case YahooFinanceTimeRange.SixMonth: return GetMonthlyStepUnits(tradingPeriod, interval);
+            case YahooFinanceTimeRange.OneYear: return GetMonthlyStepUnits(tradingPeriod, interval);
+            case YahooFinanceTimeRange.TwoYear: return GetMonthlyStepUnits(tradingPeriod, interval);
+            case YahooFinanceTimeRange.FiveYear: return GetYearlyStepUnits(tradingPeriod, interval);
+            case YahooFinanceTimeRange.TenYear: return GetYearlyStepUnits(tradingPeriod, interval);
+            case YahooFinanceTimeRange.Max: return GetYearlyStepUnits(tradingPeriod, interval);
+            case YahooFinanceTimeRange.YearToDate:
+            default:
+                if (values != null && values.Count > 0)
+                {
+                    var endTime = values[values.Count - 1].Timestamp;
+                    var startTime = values[0].Timestamp;
+                    var elapsed = endTime - startTime;
+
+                    if (elapsed.Days < 1)
+                        return GetHourlyStepUnits(interval);
+                    if (elapsed.Days <= 31)
+                        return GetDailyStepUnits(tradingPeriod, interval);
+                    if (elapsed.Days <= 730)
+                        GetMonthlyStepUnits(tradingPeriod, interval);
+                    return GetYearlyStepUnits(tradingPeriod, interval);
+                }
+                return GetHourlyStepUnits(interval);
+        }
+    }
+
     static string StockTradeTooltipFormatter(ChartPoint<StockTradePoint, BezierPoint<CircleGeometry>, LabelGeometry> point)
     {
         var dateTime = point.Model.Timestamp.ToLocalTime().ToString("dddd, MMMM dd h:mm tt");
@@ -348,57 +459,12 @@ public partial class StockDetailsPage : ContentPage
         return $"{dateTime}\r\nOpen: {open}\r\nClose: {close}\r\nHigh: {high}\r\nLow: {low}";
     }
 
-    void UpdateXAxis(YahooFinanceTimeRange range, YahooFinanceTimeInterval interval, List<StockTradePoint> values)
+    void UpdateXAxis(YahooFinanceTimeRange range, List<StockTradePoint> values, double minStep)
     {
         var gridColor = App.Current.UserAppTheme == AppTheme.Light ? SKColors.LightGray : SKColors.DarkSlateGray;
         var labelColor = App.Current.UserAppTheme == AppTheme.Light ? SKColors.Black : SKColors.White;
         var maxLimit = values != null ? values.Count : 0;
         var labeler = GetXAxesLabeler(range, values);
-        TimeSpan timeRange;
-        double minStep = 1;
-
-        if (values != null && values.Count > 0)
-        {
-            var close = values[values.Count - 1].Timestamp;
-            var open = values[0].Timestamp;
-
-            timeRange = close - open;
-        }
-        else
-        {
-            timeRange = TimeSpan.Zero;
-        }
-
-        if (timeRange.TotalDays < OneDay)
-        {
-            minStep = TimeSpan.FromHours(1).Ticks;
-        }
-        else if (timeRange.TotalDays < OneWeek)
-        {
-            minStep = TimeSpan.FromDays(OneDay).Ticks;
-        }
-        else if (timeRange.TotalDays < OneMonth)
-        {
-            minStep = TimeSpan.FromDays(OneWeek).Ticks;
-        }
-        else if (timeRange.TotalDays < OneMonth * 6)
-        {
-            minStep = TimeSpan.FromDays(OneMonth).Ticks;
-        }
-        else if (timeRange.TotalDays < OneYear * 2)
-        {
-            var months = timeRange.TotalDays / OneMonth;
-            var multiplier = months / 5;
-
-            minStep = TimeSpan.FromDays(OneMonth * multiplier).Ticks;
-        }
-        else
-        {
-            var years = timeRange.TotalDays / OneYear;
-            var multiplier = years / 5;
-
-            minStep = TimeSpan.FromDays(OneYear * multiplier).Ticks;
-        }
 
         StockPriceChart.XAxes = new[] {
             new Axis {
@@ -412,7 +478,7 @@ public partial class StockDetailsPage : ContentPage
                 //UnitWidth = timeUnit.Ticks,
                 MinLimit = 0,
                 MaxLimit = maxLimit,
-                //MinStep = minStep,
+                MinStep = minStep,
                 Labeler = labeler,
                 IsVisible = true
             }
@@ -449,9 +515,26 @@ public partial class StockDetailsPage : ContentPage
         return chart.Meta.CurrentTradingPeriod?.Regular ?? chart.Meta.TradingPeriods?.Regular[0][0];
     }
 
+    static int GetIntervalMinutes(YahooFinanceTimeInterval interval)
+    {
+        switch (interval)
+        {
+            case YahooFinanceTimeInterval.OneMinute: return 1;
+            case YahooFinanceTimeInterval.TwoMinutes: return 2;
+            case YahooFinanceTimeInterval.FiveMinutes: return 5;
+            case YahooFinanceTimeInterval.FifteenMinutes: return 15;
+            case YahooFinanceTimeInterval.ThirtyMinutes: return 30;
+            case YahooFinanceTimeInterval.SixtyMinutes:
+            case YahooFinanceTimeInterval.OneHour: return 60;
+            case YahooFinanceTimeInterval.NinetyMinutes: return 90;
+            default: return 0;
+        }
+    }
+
     void UpdateChart(YahooFinanceTimeRange range, YahooFinanceChart chart)
     {
         var regularTradingPeriod = GetRegularMarketTradingPeriod(chart);
+        var now = DateTimeOffset.UtcNow.ToOffset(regularTradingPeriod.GmtOffset);
         var tradingPeriodStartTime = regularTradingPeriod.Start.TimeOfDay;
         var tradingPeriodEndTime = regularTradingPeriod.End.TimeOfDay;
         var interval = chart.Meta.DataGranularity;
@@ -460,44 +543,67 @@ public partial class StockDetailsPage : ContentPage
         double open = 0, close = 0;
         double high = 0, low = 0;
 
-        values = new List<StockTradePoint>(quote.Close.Length);
-
-        for (int i = 0; i < quote.Close.Length; i++)
+        if (quote.Close != null)
         {
-            // Ignore null values
-            if (!quote.High[i].HasValue && !quote.Open[i].HasValue && !quote.Close[i].HasValue && !quote.Low[i].HasValue)
-                continue;
+            values = new List<StockTradePoint>(quote.Close.Length);
 
-            var timestamp = DateTimeOffset.FromUnixTimeSeconds(timestamps[i]).ToOffset(chart.Meta.GmtOffset);
-
-            // Note: Larger time intervals aren't necessarily timestamped within the regular trading period timeframe.
-            if (interval < YahooFinanceTimeInterval.OneWeek)
+            for (int i = 0; i < quote.Close.Length; i++)
             {
-                var time = timestamp.TimeOfDay;
-
-                // Ignore pre/post trading data
-                if (time < tradingPeriodStartTime || time > tradingPeriodEndTime)
+                // Ignore null values
+                if (!quote.High[i].HasValue && !quote.Open[i].HasValue && !quote.Close[i].HasValue && !quote.Low[i].HasValue)
                     continue;
+
+                var timestamp = DateTimeOffset.FromUnixTimeSeconds(timestamps[i]).ToOffset(chart.Meta.GmtOffset);
+
+                // Note: Larger time intervals aren't necessarily timestamped within the regular trading period timeframe.
+                if (interval < YahooFinanceTimeInterval.OneWeek)
+                {
+                    var time = timestamp.TimeOfDay;
+
+                    // Ignore pre/post trading data
+                    if (time < tradingPeriodStartTime || time > tradingPeriodEndTime)
+                        continue;
+                }
+
+                values.Add(new StockTradePoint(values.Count, timestamp, quote.High[i], quote.Open[i], quote.Close[i], quote.Low[i]));
+
+                if (open == 0)
+                {
+                    if (quote.Open[i].HasValue)
+                        open = quote.Open[i].Value;
+                    else if (quote.Close[i].HasValue)
+                        open = quote.Close[i].Value;
+                    low = open;
+                }
+
+                if (quote.High[i].HasValue)
+                    high = Math.Max(high, quote.High[i].Value);
+
+                if (quote.Low[i].HasValue)
+                    low = Math.Min(low, quote.Low[i].Value);
+
+                if (quote.Close[i].HasValue)
+                    close = quote.Close[i].Value;
             }
 
-            values.Add(new StockTradePoint(values.Count, timestamp, quote.High[i], quote.Open[i], quote.Close[i], quote.Low[i]));
+            if (range == YahooFinanceTimeRange.OneDay)
+            {
+                // Fill the remainder of the day with null values
+                var timestamp = values[values.Count - 1].Timestamp;
+                var endTime = regularTradingPeriod.End.TimeOfDay;
+                var seconds = GetIntervalMinutes(interval) * 60;
 
-            if (open == 0) {
-                if (quote.Open[i].HasValue)
-                    open = quote.Open[i].Value;
-                else if (quote.Close[i].HasValue)
-                    open = quote.Close[i].Value;
-                low = open;
+                while (timestamp.TimeOfDay < endTime)
+                {
+                    timestamp = timestamp.AddSeconds(seconds);
+
+                    values.Add(new StockTradePoint(values.Count, timestamp, null, null, null, null));
+                }
             }
-
-            if (quote.High[i].HasValue)
-                high = Math.Max(high, quote.High[i].Value);
-
-            if (quote.Low[i].HasValue)
-                low = Math.Min(low, quote.Low[i].Value);
-
-            if (quote.Close[i].HasValue)
-                close = quote.Close[i].Value;
+        }
+        else
+        {
+            values = new List<StockTradePoint>();
         }
 
         var color = close >= open ? SKColors.Green : SKColors.Red;
@@ -513,6 +619,7 @@ public partial class StockDetailsPage : ContentPage
                 EnableNullSplitting = false,
                 GeometryFill = null,
                 GeometrySize = 0,
+                LineSmoothness = 0,
                 Mapping = (trade, point) =>
                 {
                     point.SecondaryValue = trade.Index;
@@ -526,7 +633,9 @@ public partial class StockDetailsPage : ContentPage
             }
         };
 
-        UpdateXAxis(range, interval, values);
+        var minStep = GetXAxesMinStep(regularTradingPeriod, range, interval, values);
+
+        UpdateXAxis(range, values, minStep);
         UpdateYAxis(low, high);
     }
 
